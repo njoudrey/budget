8118daadc3ca94290c721bba963bf2e239262da5
Nicholas Joudrey <45206478+njoudrey@users.noreply.github.com>
2022-05-03 17:41:27 -0400

    moved database setup out of transaction route, setup form to get transaction details, setup the structure for adding to firestore

--- database.js
+++ database.js
@@ -6,4 +6,4 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-export const db = admin.firestore();
+module.exports = admin.firestore();
--- routes/transaction.js
+++ routes/transaction.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require("/Users/nicholasjoudrey/firestore/budget-a5c27-firebase-adminsdk-mt5q8-dcad41886c");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
+var db = require('../database');
 
 
 /* POST transaction entry. */
 router.post('/', (req, res) => {
-  db.collection('transactions').doc('nick').set(req.body);
-  res.send('got transaction POST request');
+  db.collection('users').doc('nick').collection('transactions').doc().set(req.body);
+  res.render('index', { title: 'Budget', transtat: 'Transaction submitted successfully' });
 });
 
 module.exports = router;
